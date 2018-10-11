e8d671356bce1b330983885370980b2394587472
Anand Nigam <anandnigam.2aug@gmail.com>
2018-10-11 12:05:01 +0530

    Initial Commit

---
+++ CameraFrameExtractor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraFrameExtractor
+//
+//  Created by Anand Nigam on 11/10/18.
+//  Copyright © 2018 Anand Nigam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
